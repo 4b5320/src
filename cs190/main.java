38b776da46131cdddecae1913e0f9e9d5daf4799
israel <>
2019-01-13 13:10:20 +0800

    update

--- cs190/main.java
+++ cs190/main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class main{
 
 	public static void main(String[] arguments) {
-		arguments = new String[] {"A", "12", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"};
+		arguments = new String[] {"A", "9"};
 		
 		if (!arguments[0].equals("-fitness")) {
 			LinkedList<String> done = new LinkedList<String>();
